f9027bfef3f9d91af489b94c4d166e6b00a971a3
1010Gabriel0101 <148381895+1010Gabriel0101@users.noreply.github.com>
2023-10-20 20:18:20 -0600

    Actualizar Actividad 1.c

--- Actividad 1.c
+++ Actividad 1.c
@@ -1,1 +1,17 @@
-jj
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+FILE *archivo;
+char caracter;
+archivo = fopen("prueba.txt","r");
+}
+
+/*
+r lectura
+w escritura
+a abrir un archivo y empieza a escribir donde se quedó, si no existe lo crea
+r+ lectura y escritura, el archivo debe existir
+w+ crea un archivo de lectura y escritura se crea si no existe, si existe, lo sobreescribe
+w+ crea un archivo de lectura escritura se crea si no existe o lo sobreescribe 
+*/
